44e529af915ea3853c94dcaa7344f1eec62efabe
Valerie <valeriec@san.rr.com>
2015-01-15 10:46:00 -0800

    templates

--- wp-content/themes/awesome/functions.php
+++ wp-content/themes/awesome/functions.php
@@ -2,5 +2,12 @@
 //turn on some sleeping features
 add_theme_support('post-thumbnails');
 
+//make forms follow new HTML5 guidelines
+add_theme_support('html5', array('search-form', 'comment-list', 'comment-form',
+								  'gallery', 'caption') );
+
+//make additional image sizes
+add_image_size( 'big-banner', '1300', '300', true );
+
 
 //no close PHP
---
+++ wp-content/themes/awesome/page.php
@@ -0,0 +1,38 @@
+<?php get_header(); //include header.php ?>
+
+<main id="content">
+	<?php //THE LOOP
+		if( have_posts() ): ?>
+		<?php while( have_posts() ): the_post(); ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class();//this adds extra classes to the post ?>>
+			<h2 class="entry-title"> 
+				<a href="<?php the_permalink(); ?>"> 
+					<?php the_title(); ?> 
+				</a>
+			</h2>
+			<?php the_post_thumbnail('thumbnail'); ?>
+			<div class="entry-content">
+				<?php //if showing a single post or page, display all the content... otherwise show the short content		the_excerpt(); //shows an excerpt of the content
+					if( is_singular() ){
+						the_content();
+					}else{
+						the_excerpt();
+					}
+				?> 
+			</div>
+			
+		</article><!-- end post -->
+
+		<?php endwhile; ?>
+	<?php else: ?>
+
+	<h2>Sorry, no posts found</h2>
+	<p>Try using the search bar instead</p>
+
+	<?php endif;  //end THE LOOP ?>
+
+</main><!-- end #content -->
+
+<?php get_sidebar(); //include sidebar.php ?>
+<?php get_footer(); //include footer.php ?>
